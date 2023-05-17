7bbaf9499ec042d9dcaf5133cf54646bb6938409
Karol Kozłowski <karol.z.kozlowski@gmail.com>
2023-05-18 01:33:11 +0200

    Use external logger

--- server.go
+++ server.go
@@ -1,22 +1,12 @@
 package main
 
 import (
-	"log"
 	"net/http"
 	"os"
-)
 
-var (
-	WarningLogger *log.Logger
-	InfoLogger    *log.Logger
-	ErrorLogger   *log.Logger
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 )
-
-func init() {
-	InfoLogger = log.New(os.Stdout, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
-	WarningLogger = log.New(os.Stderr, "WARNING: ", log.Ldate|log.Ltime|log.Lshortfile)
-	ErrorLogger = log.New(os.Stderr, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile)
-}
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
 	var path = r.URL.Path
@@ -25,14 +15,14 @@ func rootHandler(w http.ResponseWriter, r *http.Request) {
 	shortcuts["/dotnot"] = "https://dotnot.pl"
 	shortcuts["/wp"] = "https://wp.pl"
 
-	WarningLogger.Printf("Received request %s%s\n", r.Host, path)
+	log.Warn().Msg("Received request: " + r.Host + path)
 
 	target, ok := shortcuts[path]
 	if ok {
-		InfoLogger.Printf("Redirecting -> %s\n", target)
+		log.Info().Msg("Redirecting -> " + target)
 		http.Redirect(w, r, target, http.StatusSeeOther)
 	} else {
-		WarningLogger.Printf("Received request %s%s\n", r.Host, path)
+		log.Warn().Msg("Redirect not found")
 	}
 }
 
@@ -40,8 +30,11 @@ func main() {
 	// Options
 	portNumber := "9000"
 
+	zerolog.SetGlobalLevel(zerolog.InfoLevel)
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+
 	// main handler
 	http.HandleFunc("/", rootHandler)
-	InfoLogger.Println("Server listening on port ", portNumber)
+	log.Info().Msg("Server listening on port " + portNumber)
 	http.ListenAndServe(":"+portNumber, nil)
 }
