acb148d327d83a8c8b9b85fa9f1e1307ca374fad
Karol Kozłowski <karol.z.kozlowski@gmail.com>
2023-05-19 22:18:57 +0200

    Add comment

--- server.go
+++ server.go
@@ -20,6 +20,7 @@ func main() {
 	// Options
 	portNumber := "9000"
 
+	// setup logger
 	zerolog.SetGlobalLevel(zerolog.DebugLevel)
 	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
 
@@ -34,6 +35,7 @@ func main() {
 	http.HandleFunc("/", redirector.RootHandler)
 
 	log.Info().Msg("Server listening on port " + portNumber)
+
 	http.ListenAndServe(":"+portNumber, nil)
 
 	log.Info().Msg("Server shutting down")
