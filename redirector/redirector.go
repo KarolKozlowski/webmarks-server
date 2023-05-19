6b298020ea63be764ecc6bfc657dd01d322a38c5
Karol Kozłowski <karol.z.kozlowski@gmail.com>
2023-05-19 15:33:07 +0200

    Add landing page

--- redirector/redirector.go
+++ redirector/redirector.go
@@ -16,8 +16,12 @@ type PageData struct {
 	Title string
 }
 
+type Shortcut struct {
+	Target string
+}
+
 func RootHandler(w http.ResponseWriter, r *http.Request) {
-	var path = r.URL.Path
+	var path = strings.Replace(r.URL.Path, "/", "", 1)
 	var allFiles []string
 
 	template_dir := os.DirFS("./templates")
@@ -38,24 +42,34 @@ func RootHandler(w http.ResponseWriter, r *http.Request) {
 		log.Error().Msg(err.Error())
 	}
 
-	shortcuts := make(map[string]string)
-	shortcuts["/dotnot"] = "https://dotnot.pl"
-	shortcuts["/wp"] = "https://wp.pl"
+	if path != "" {
+		var shortcuts = map[string]Shortcut{}
+		shortcuts["dotnot"] = Shortcut{Target: "https://dotnot.pl"}
+		shortcuts["wp"] = Shortcut{Target: "https://wp.pl"}
 
-	log.Warn().Msg("Received request: " + r.Host + path)
+		log.Warn().Msg("Received request: " + r.Host + path)
 
-	target, ok := shortcuts[path]
-	if ok {
-		log.Info().Msg("Redirecting -> " + target)
-		http.Redirect(w, r, target, http.StatusSeeOther)
+		shortcut, ok := shortcuts[path]
+		if ok {
+			log.Info().Msg("Redirecting " + path + " -> " + shortcut.Target)
+			http.Redirect(w, r, shortcut.Target, http.StatusSeeOther)
+		} else {
+			log.Warn().Msg("Redirect not found")
+
+			data := PageData{
+				Title: "Redirect: " + path + " not found!",
+			}
+
+			s1 := templates.Lookup("main.tmpl")
+			s1.Execute(w, data)
+		}
 	} else {
-		log.Warn().Msg("Redirect not found")
-
 		data := PageData{
-			Title: "Redirect not found!",
+			Title: "Welcome!",
 		}
 
 		s1 := templates.Lookup("main.tmpl")
 		s1.Execute(w, data)
 	}
+
 }
