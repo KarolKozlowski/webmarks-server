087134799cd32f374569add8d7af4b251a2714f6
Karol Kozłowski <karol.z.kozlowski@gmail.com>
2023-05-19 19:54:38 +0200

    Allow serving static files

--- server.go
+++ server.go
@@ -10,13 +10,27 @@ import (
 	"github.com/rs/zerolog/log"
 )
 
+func redirectToStatic(w http.ResponseWriter, r *http.Request) {
+	urlBase := r.URL.Path
+	log.Debug().Msg("Redirect to /static/: " + urlBase + " -> " + "/static" + r.URL.Path)
+	http.Redirect(w, r, "/static"+r.URL.Path, http.StatusSeeOther)
+}
+
 func main() {
 	// Options
 	portNumber := "9000"
 
-	zerolog.SetGlobalLevel(zerolog.InfoLevel)
+	zerolog.SetGlobalLevel(zerolog.DebugLevel)
 	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
 
+	// handle static files
+	fs := http.FileServer(http.Dir("./static"))
+	http.Handle("/static/", http.StripPrefix("/static", fs))
+
+	// redirect root static resources
+	http.HandleFunc("/favicon.ico", redirectToStatic)
+
+	// main handler
 	http.HandleFunc("/", redirector.RootHandler)
 
 	log.Info().Msg("Server listening on port " + portNumber)
